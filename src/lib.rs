a11933885f528021b7aabe89606e0ce7287ffebe
ringsaturn <ringsaturn.me@gmail.com>
2022-12-01 00:52:34 +0800

    update Rust port

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::vec;
+
+use lazy_static::lazy_static;
+
+use serde::{Deserialize, Serialize};
+
+lazy_static! {
+    pub static ref CITIES: Cities = get_cities();
+}
+
+/// ```rust
+/// use cities_json::CITIES;
+/// println!("first {:?}", CITIES.get(0).unwrap().name);
+/// ```
+pub type Cities = Vec<City>;
+
+pub struct City {
+    pub country: String,
+    pub name: String,
+    pub lat: f64,
+    pub lng: f64,
+}
+
+type RawCities = Vec<RawCity>;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct RawCity {
+    country: String,
+    name: String,
+    lat: String,
+    lng: String,
+}
+
+fn load_json_data() -> Vec<u8> {
+    include_bytes!("../upstream/cities.json").to_vec()
+}
+
+fn get_cities() -> Cities {
+    let raw_cities: RawCities = serde_json::from_slice(&load_json_data()[..]).unwrap();
+    let mut cities: Cities = vec![];
+    for rawcity in raw_cities.iter() {
+        cities.push(City {
+            country: rawcity.country.to_owned(),
+            name: rawcity.name.to_owned(),
+            lat: rawcity.lat.parse::<f64>().unwrap(),
+            lng: rawcity.lng.parse::<f64>().unwrap(),
+        })
+    }
+    return cities;
+}
