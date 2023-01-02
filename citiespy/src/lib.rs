1cd2a29b0fdd48af18ee083d247e3fe735050192
ringsaturn <ringsaturn.me@gmail.com>
2023-01-02 13:05:02 +0800

    support fetch all (#11)

--- citiespy/src/lib.rs
+++ citiespy/src/lib.rs
@@ -1,4 +1,5 @@
 use cities_json::get_random_cities;
+use cities_json::CITIES as RUST_CITIES;
 use pyo3::prelude::*;
 
 #[pyclass]
@@ -27,20 +28,38 @@ impl City {
     }
 }
 
+pub type Cities = Vec<City>;
+
 #[pyfunction]
 pub fn random_city() -> PyResult<City> {
-    let raw_city = get_random_cities();
+    let rust_city = get_random_cities();
     let city = City {
-        country: raw_city.country.to_string(),
-        lat: raw_city.lat,
-        lng: raw_city.lng,
-        name: raw_city.name.to_string(),
+        country: rust_city.country.to_string(),
+        lat: rust_city.lat,
+        lng: rust_city.lng,
+        name: rust_city.name.to_string(),
     };
     return Ok(city);
 }
 
+#[pyfunction]
+pub fn all_cities() -> PyResult<Cities> {
+    let mut cities: Cities = vec![];
+    for rust_city in RUST_CITIES.iter(){
+        cities.push(City {
+            country: rust_city.country.to_string(),
+            lat: rust_city.lat,
+            lng: rust_city.lng,
+            name: rust_city.name.to_string(),
+        })
+    }
+    return Ok(cities);
+}
+
+
 #[pymodule]
 fn citiespy(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(random_city, m)?)?;
+    m.add_function(wrap_pyfunction!(all_cities, m)?)?;
     Ok(())
 }
--- citiespy/tests/test_basic.py
+++ citiespy/tests/test_basic.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
 
-from citiespy import random_city
+from citiespy import all_cities, random_city
 
 
 class TestCities(TestCase):
-    def test_random(self):
+    def test_get_random(self):
         _ = random_city()
+
+    def test_get_all(self):
+        cities = all_cities()
+        self.assertGreater(len(cities), 100000)
