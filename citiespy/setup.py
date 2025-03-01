b38149cd4be5311ad8f3728d76e0d9bbaf96ae5a
ringsaturn <ringsaturn.me@gmail.com>
2025-03-01 12:12:41 +0800

    use uv to manage python binding

--- citiespy/setup.py
+++ citiespy/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(
     name="citiespy",
-    url="https://github.com/ringsaturn/go-cities.json/blob/main/citiespy"
+    url="https://github.com/ringsaturn/go-cities.json/blob/main/citiespy",
 )
